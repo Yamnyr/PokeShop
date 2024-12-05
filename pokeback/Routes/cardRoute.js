3f1e6bd2141180a7286d05a3c32c1a32e0d427a2
Yamnyr <quentinwaroquet@gmail.com>
2024-12-05 17:05:28 +0100

    routes ++

--- pokeback/Routes/cardRoute.js
+++ pokeback/Routes/cardRoute.js
@@ -1,12 +1,16 @@
 
 const express = require('express');
 const router = express.Router();
-const {updateCard, deleteCard, getCards} = require("../Controllers/cardController");
+const {updateCard, deleteCard, getCards, getCard, getCardByUser, createCard} = require("../Controllers/cardController");
 const authMiddleware = require("../Middleware/authMiddleware");
 
 
+router.post('/create', authMiddleware, createCard);
 router.put("/update/:id", authMiddleware, updateCard);
 router.delete("/delete/:id", authMiddleware, deleteCard);
-router.get("/getall", authMiddleware, getCards);
+router.get("/getall", getCards);
+router.get("/getone/:id", getCard);
+router.get("/getonebuuser/:id", getCardByUser);
+
 
 module.exports = router;
--- pokeback/Routes/typeRoute.js
+++ pokeback/Routes/typeRoute.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
 const router = express.Router();
-const {updateType, deleteType, getType} = require("../Controllers/typeController");
+const {updateType, deleteType, getType, createType} = require("../Controllers/typeController");
 const authMiddleware = require("../Middleware/authMiddleware");
 
 
+router.post('/create', authMiddleware, createType);
 router.put("/update/:id", authMiddleware, updateType);
 router.delete("/delete/:id", authMiddleware, deleteType);
-router.get("/getall", authMiddleware, getType);
+router.get("/getall", getType);
 
 module.exports = router;
