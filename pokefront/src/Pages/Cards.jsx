11d47f8b06af68f9fffab7e1e662921dc73c8a97
Yamnyr <quentinwaroquet@gmail.com>
2024-12-06 11:38:53 +0100

    card / api

--- pokefront/src/Pages/Cards.jsx
+++ pokefront/src/Pages/Cards.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import Card from "../Components/Card";
 import { cardService } from "../Services/cardsApi";
+import PokemonCard from "../Components/Card";
+import card from "../Components/Card";
 
 const Cards = () => {
-  const [cards, setCards] = useState([]); // Initialisation vide
+  const [cards, setCards] = useState([]);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -12,7 +13,7 @@ const Cards = () => {
     setError(null);
     try {
       const response = await cardService.fetchCards();
-      setCards(response.cards); // On utilise uniquement la propriété "cards" de la réponse
+      setCards(response.cards);
     } catch (error) {
       setError(error.message || "Une erreur est survenue");
     } finally {
@@ -23,16 +24,16 @@ const Cards = () => {
   useEffect(() => {
     getCards();
   }, []);
-
+  console.log(cards)
   return (
-    <div>
-      <h1>Cards</h1>
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      {!isLoading && cards.length === 0 && <p>No cards available.</p>}
-      {cards.length > 0 &&
-        cards.map((card) => <Card key={card._id} card={card} />)}
-    </div>
+      <div>
+        <h1>Cards</h1>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Error: {error}</p>}
+        {!isLoading && cards.length === 0 && <p>No cards available.</p>}
+        {cards.length > 0 &&
+            cards.map((card) => <PokemonCard key={card._id} card={card} />)}
+      </div>
   );
 };
 
